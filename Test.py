5c9a7f9d14845b072c19fd40db3d4400abddb117
sid-dev-hub <123942006+sid-dev-hub@users.noreply.github.com>
2023-07-15 14:11:22 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,7 +1,24 @@
 
-t  = 1
 
-def dum():
-    print(10+t)
 
-dum()
+def fibo(n):
+        print("Inside Fibo", __name__)
+        p1 = 0
+        p2 = 1
+        sum = 0
+        rs = [0,1]
+        while sum < n:
+                   
+            sum = p1 + p2
+            if sum > n:
+                break
+            rs.append(sum)
+            
+            p1 = p2
+            p2 = sum
+        return rs 
+            
+
+
+
+dum(10)
